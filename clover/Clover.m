3fa8e8c7f208c6ebe753f16bfb85fc9f35e43064
Marcus Westin <marcus.westin@gmail.com>
2012-01-16 10:47:01 -0800

    Assert that there is a custom URI scheme registered, and register one for the example app

--- clover/Clover.m
+++ clover/Clover.m
@@ -18,8 +18,11 @@
     state.accountID = accountID;
     state.callbackID = 0;
     state.callbacks = [NSMutableDictionary dictionary];
-    state.appUrlScheme = [CloverState getCurrentAppURLScheme];
     state.mac = [CloverState getMac];
+    state.appUrlScheme = [CloverState getCurrentAppURLScheme];
+    if (!state.appUrlScheme || [state.appUrlScheme length] == 0) {
+        [NSException raise:@"Custom App URL Scheme Required" format:@"You must register a custom URL scheme in your app in order to receive messages back from the Clover App - see https://www.clover.com/docs/ios-sdk"];
+    }
 }
 
 + (CloverButton *)createButtonWithAmount:(NSString *)amount title:(NSString *)title {
